04d12517f80724b3227ed000b217e29de44c9012
Terrance Corley <terrancecorley@outlook.com>
2018-05-07 17:46:10 -0700

    added assertion cases for user POST endpoint

--- routes/users.js
+++ routes/users.js
@@ -18,7 +18,7 @@ router.post('/', (req, res, next) => {
   }
 
   const stringFields = ['username', 'fullName', 'password'];
-  const notString = stringFields.find(field => typeof req.body[field] !== 'string');
+  const notString = stringFields.find(field => field in req.body && typeof req.body[field] !== 'string');
 
   if (notString) {
     const err = new Error(`'${notString}' is not a string value`);
@@ -26,7 +26,48 @@ router.post('/', (req, res, next) => {
     return next(err);
   }
 
-  const { username, password, fullName } = req.body;
+  const explicityTrimmedFields = ['username', 'password'];
+  const nonTrimmedField = explicityTrimmedFields.find(
+    field => req.body[field].trim() !== req.body[field]
+  );
+
+  if (nonTrimmedField) {
+    const err = new Error(`Field: '${nonTrimmedField}' cannot start or end with whitespace`);
+    err.status = 422;
+    return next(err);
+  }
+
+  const sizedFields = {
+    username: { min: 1 },
+    password: { min: 8, max: 72 }
+  };
+
+  const tooSmallField = Object.keys(sizedFields).find(
+    field => 'min' in sizedFields[field] &&
+      req.body[field].trim().length < sizedFields[field].min
+  );
+
+  if (tooSmallField) {
+    const min = sizedFields[tooSmallField].min;
+    const err = new Error(`Field: '${tooSmallField}' must be at least ${min} characters long`);
+    err.status = 422;
+    return next(err);
+  }
+
+  const tooLargeField = Object.keys(sizedFields).find(
+    field => 'max' in sizedFields[field] &&
+      req.body[field].trim().length > sizedFields[field].max
+  );
+
+  if (tooLargeField) {
+    const max = sizedFields[tooLargeField].max;
+    const err = new Error(`Field: '${tooSmallField}' must be at most ${max} characters long`);
+    err.status = 422;
+    return next(err);
+  }
+
+  let { username, password, fullName = '' } = req.body;
+  fullName = fullName.trim();
 
   return User.hashPassword(password)
     .then(digest => {
